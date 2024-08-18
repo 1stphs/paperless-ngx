cba31c88956cd6fa09b6553ae6901a6038a48085
Xu Ke <kxu1988@gmail.com>
2024-08-18 08:37:59 +0800

    Enhancement: Allow setting 'X_FRAME_OPTIONS' via environment variable

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -509,8 +509,8 @@ def _parse_remote_user_settings() -> str:
 HTTP_REMOTE_USER_HEADER_NAME = _parse_remote_user_settings()
 
 # X-Frame options for embedded PDF display:
-X_FRAME_OPTIONS = "ANY" if DEBUG else "SAMEORIGIN"
-
+# X_FRAME_OPTIONS = "ANY" if DEBUG else "SAMEORIGIN"
+X_FRAME_OPTIONS = "ANY" 
 
 # The next 3 settings can also be set using just PAPERLESS_URL
 CSRF_TRUSTED_ORIGINS = __get_list("PAPERLESS_CSRF_TRUSTED_ORIGINS")
